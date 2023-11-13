b5c114a3d3e5bd43f9f4a23e197c9342f7d7f222
Guedes <albertoguedes4@gmail.com>
2023-11-13 17:56:23 +0000

    WeatherProvider/OpenWeatherMapProvider commit

--- src/main/java/org/ulpgc/dacd/control/WeatherProvider.java
+++ src/main/java/org/ulpgc/dacd/control/WeatherProvider.java
@@ -1,4 +1,9 @@
 package org.ulpgc.dacd.control;
 
+import org.ulpgc.dacd.model.WeatherData;
+
+import java.io.IOException;
+
 public interface WeatherProvider {
+	WeatherData getWeatherData(double latitude, double longitude) throws IOException;
 }
